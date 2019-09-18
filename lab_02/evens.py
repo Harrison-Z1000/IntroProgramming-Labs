82a2cdcc1d4b168060af99161a06fdce6f08444f
Harrison Zheng <harrison.zheng1@marist.edu>
2019-09-17 22:50:12 -0400

    Evens Loop Exercise
    Solution to evens loop exercise.

---
+++ lab_02/evens.py
@@ -0,0 +1,10 @@
+# Introduction to Programming
+# Author: Harrison Zheng
+# Date: 09/17/19
+# This program prints all the even integers from 2 through 20.
+
+
+def main():
+    for i in range(2, 21, 2):
+        print(i)
+main()
