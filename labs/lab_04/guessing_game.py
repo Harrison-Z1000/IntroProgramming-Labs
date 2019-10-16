f8f5600886014a4acec9b5936f6978b743462066
Harrison Zheng <harrison.zheng1@marist.edu>
2019-10-16 04:49:35 -0400

    Further Improved guessing_game.py
    Usability improvements to guessing game. Resolves issues #4 and #5.

--- labs/lab_04/guessing_game.py
+++ labs/lab_04/guessing_game.py
@@ -7,16 +7,23 @@
 def main():
     animal = "penguin"
     userAnimal = "some animal"
+    print("Enter a command that begins with 'q' any time to quit the game. \n")
     while userAnimal != animal:
-        print("Enter 'Quit' at any time to quit the game. \n")
         print("The computer is thinking of an animal.")
         userAnimal = str(input("What kind of animal do you think it is?: "))
         userAnimal = userAnimal.lower()  # User's input is converted to all lowercase letters to make comparing easier
-        if userAnimal == animal:  # If the user guesses correctly, the program breaks out of the while loop.
+        if userAnimal[0] == "q":
+            break
+        elif userAnimal == animal:  # If the user guesses correctly, the program asks them whether they like the
+            # animal and prints a comment before breaking out of the loop.
             print("Congratulations! You are correct!")
-            break
-        elif userAnimal == "quit":
-            break
+            usersOpinion = str(input("Do you like the " + animal + "? (Enter 'y' or 'n'): "))
+            if usersOpinion == "y":
+                print("Awesome! The " + animal + " is a fascinating creature.")
+                break
+            else:
+                print("That's unfortunate. The " + animal + " is such a cool animal.")
+                break
         else:  # If the user guesses incorrectly, they are prompted to guess again.
             print("Sorry, that is incorrect. Please try again.")
             userAnimal = str(input("What kind of animal do you think it is?: "))
