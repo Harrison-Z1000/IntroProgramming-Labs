05a9fcaca478b9ecc703cc63afacbd94e8c1643a
Harrison Zheng <harrison.zheng1@marist.edu>
2019-10-16 03:31:45 -0400

    Improved guessing_game.py
    The game now gives users the option to exit at any time. It also takes answers regardless of what case it is in.

--- labs/lab_04/guessing_game.py
+++ labs/lab_04/guessing_game.py
@@ -1,17 +1,21 @@
 # Introduction to Programming
 # Author: Harrison Zheng
-# Date: 10/3/19
+# Date: 10/16/19
 # This is a game where the user tries to guess the animal that the programmer was thinking of.
 
 
 def main():
-    animal = "Penguin"
-    userAnimal = "Something"
+    animal = "penguin"
+    userAnimal = "some animal"
     while userAnimal != animal:
+        print("Enter 'Quit' at any time to quit the game. \n")
         print("The computer is thinking of an animal.")
         userAnimal = str(input("What kind of animal do you think it is?: "))
+        userAnimal = userAnimal.lower()  # User's input is converted to all lowercase letters to make comparing easier
         if userAnimal == animal:  # If the user guesses correctly, the program breaks out of the while loop.
             print("Congratulations! You are correct!")
+            break
+        elif userAnimal == "quit":
             break
         else:  # If the user guesses incorrectly, they are prompted to guess again.
             print("Sorry, that is incorrect. Please try again.")
@@ -19,5 +23,4 @@ def main():
 
 
 main()
-
 
