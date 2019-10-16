f54ea573305cdb3354c580aab04bf26b42384304
Harrison Zheng <harrison.zheng1@marist.edu>
2019-10-16 08:27:50 -0400

    Further Improved credentials.py
    Modified the credentials.py program so that it generates usernames in all lowercase characters and requires passwords to have both upper and lowercase characters

--- labs/lab_05/credentials.py
+++ labs/lab_05/credentials.py
@@ -7,34 +7,40 @@
 def main():
     names = getNames()
     uname = createUsername(names)
-    passwd = getPassword()
+    getPassword()
     print("Account configured. Your new email address is",
           uname + "@marist.edu")
 
 
 def getNames():
-    # get user's first and last names
+    # Get user's first and last names
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
     return [first, last]
 
 
 def createUsername(names):
-    uname = names[0] + "." + names[1]  # Generates a Marist-style username
+    uname = names[0] + "." + names[1]  # Generate a Marist-style username
+    uname = uname.lower()
     return str(uname)
 
 
 def getPassword():
     passwd = input("Create a new password: ")  # Ask user to create a new password
-    # Ensures the password has at least 8 characters
+    checkPassword(passwd)
+    return passwd
+
+
+def checkPassword(passwd):
+    # Ensure the password has at least 8 characters and contains both upper and lowercase ones
     while len(passwd) != 0:
-        if len(passwd) < 8:
+        if len(passwd) < 8 or passwd.lower() == passwd or passwd.upper() == passwd:
             print("Fool of a Took! That password is feeble!")
             passwd = input("Create a new password: ")
         else:
             print("The force is strong in this one...")
             break
-    return passwd
+    return True
 
 
 main()
