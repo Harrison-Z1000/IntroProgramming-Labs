f99cc3cb85f84e66dfb4ab3c234aefabe159e651
Harrison Zheng <harrison.zheng1@marist.edu>
2019-10-20 22:02:38 -0400

    Moved Labs 1 & 2
    Moved lab01 and lab_02 to labs folder

---
+++ labs/lab_02/rover.py
@@ -0,0 +1,16 @@
+# Introduction to Programming
+# Author: Harrison Zheng
+# Date: 09/17/19
+# This program calculates how long it takes a photo from Curiosity to 
+# reach NASA when Mars is at its closest orbit to Earth.
+
+
+def main():
+    speed = 186000
+    distance = 34000000
+    time = distance / speed
+    print("The time it takes for a photo from Curiosity to reach NASA ", end='')
+    print("when Mars is at its closest orbit to Earth is", time, "seconds or", time / 60, "minutes.")
+
+
+main()
