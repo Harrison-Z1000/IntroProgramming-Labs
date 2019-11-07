0c0edd7b5ecdc33e1fb99b5367e8aa6967e02339
Harrison Zheng <harrison.zheng1@marist.edu>
2019-11-06 21:21:19 -0500

    Initial version of tictactoe.py
    This version runs the game, but does not check for a winner.

---
+++ labs/lab_07/tictactoe.py
@@ -0,0 +1,78 @@
+# CMPT 120 Intro to Programming
+# Lab #7 - Tic-Tac-Toe Game
+# Author: Harrison Zheng
+# Date: 2019-11-6
+
+
+def printRow(board, row):
+    # Prints a row of the board based on the value in each square
+    print("|", end='')
+    for square in range(len(board[row])):
+        numberInSquare = board[row][square]
+        if numberInSquare == 0:
+            print("   ", end='|')
+        elif numberInSquare == 1:
+            print(" x ", end='|')
+        elif numberInSquare == 2:
+            print(" o ", end='|')
+        else:
+            print("Invalid input")
+    print(" ")
+    pass
+
+
+def printBoard(board):
+    # Prints the board one row at a time with a border above and below each
+    topBorder = "+-----------+"
+    print(topBorder)
+    for row in range(len(board)):
+        printRow(board, row)
+        print(topBorder)
+    pass
+
+
+def markBoard(board, row, col, player):
+    # Checks whether the desired square is blank
+    if board[row][col] == 0:
+        board[row][col] = player
+    else:
+        print("This square is taken.")
+        # If the square is filled, the computer will ask the user to pick another one
+        row, col = getPlayerMove()
+        markBoard(board, row, col, player)
+    pass
+
+
+def getPlayerMove():
+    row = int(input("Enter the row number (the tic-tac-toe board is 3X3): "))
+    column = int(input("Enter the column number (the tic-tac-toe board is 3X3): "))
+    # Most people will enter '1' and not '0' when referring to the first row so 1 needs to be subtracted from the
+    # user's input
+    return (row - 1), (column - 1)
+
+
+def hasBlanks(board):
+    # Checks if there are any blank squares on the board
+    for row in range(len(board)):
+        for square in range(len(board[row])):
+            numberInSquare = board[row][square]
+            if numberInSquare == 0:
+                return True
+            elif board[0][0] != 0:
+                continue
+            else:
+                return False
+
+
+def main():
+    board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    player = 1
+    while hasBlanks(board):
+        printBoard(board)
+        row, col = getPlayerMove()
+        markBoard(board, row, col, player)
+        # Switches player for next turn
+        player = player % 2 + 1
+
+
+main()
